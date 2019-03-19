32e53145d5f2a30db387dbec176ff7ebd5d3508b
Melika-Emami <melika.emami30@yahoo.com>
2019-03-19 22:55:57 +0330

    Add files via upload

--- test.c
+++ test.c
@@ -1,17 +1,18 @@
 #include "test.h"
 
-void add (uint64_t *a, uint64_t *b, uint64_t *c){
-	(*c) = (*a) * (*b); 
+void add (uint16_t *a, uint16_t *b, uint16_t *c)
+{
+	(*c) = (*a) + (*b);
 }
 
-__asm void MUL(int32_t *a, int32_t *b, int32_t *c){
+__asm void MUL_4(int32_t *a, int32_t *b, int32_t *c){
 				LDR      r0,[r0,#0]
         LDR      r1,[r1,#0]
         MULS     r0,r1,r0
         STR      r0,[r2,#0]
         BX       lr
 }
-__asm void SUM(int32_t *a, int32_t *b, int32_t *c){
+__asm void SUM_4(int32_t *a, int32_t *b, int32_t *c){
 				
 				LDR      r0,[r0,#0]
         LDR      r1,[r1,#0]
@@ -20,7 +21,7 @@ __asm void SUM(int32_t *a, int32_t *b, int32_t *c){
         BX       lr
 }
 
-__asm void MIN(int32_t *a, int32_t *b, int32_t *c){
+__asm void MIN_4(int32_t *a, int32_t *b, int32_t *c){
 				LDR      r0,[r0,#0]
         LDR      r1,[r1,#0]
         SUB      r0,r0,r1
@@ -28,7 +29,7 @@ __asm void MIN(int32_t *a, int32_t *b, int32_t *c){
         BX       lr
 }
 
-__asm void DIV(int32_t *a, int32_t *b, int32_t *c){
+__asm void DIV_4(int32_t *a, int32_t *b, int32_t *c){
 				LDR      r0,[r0,#0]
         LDR      r1,[r1,#0]
         UDIV      r0,r0,r1
@@ -37,7 +38,7 @@ __asm void DIV(int32_t *a, int32_t *b, int32_t *c){
 }
 
 
-
+/*
 __asm void MUL_9(int32_t *a, int32_t *b, int32_t *c)
 {
 	LDR      r3,[r0,#0]
@@ -52,7 +53,7 @@ __asm void MUL_9(int32_t *a, int32_t *b, int32_t *c)
 	STR			 r3,[r2,#0]
 	STR			 r4,[r2,#4]
 	STR			 r5,[r2,#8]
-}
+}*/
 
 __asm void SUM_9(int32_t *a, int32_t *b, int32_t *c)
 {
@@ -71,3 +72,30 @@ __asm void SUM_9(int32_t *a, int32_t *b, int32_t *c)
 }
 
 
+__asm void MIN_9(int32_t *a, int32_t *b, int32_t *c)
+{
+	LDR      r3,[r0,#0]
+	LDR      r4,[r0,#4]
+	LDR      r5,[r0,#8]
+	LDR      r6,[r1,#0]
+	LDR      r7,[r1,#4]
+	LDR      r8,[r1,#8]
+	SUB		   r3,r3,r6
+	SBC			 r4,r4,r7
+	SBC			 r5,r5,r8
+	STR			 r3,[r2,#0]
+	STR			 r4,[r2,#4]
+	STR			 r5,[r2,#8]
+}
+
+__asm void MAC_4(int32_t *a, int32_t *b, int32_t *c)
+{
+	LDRH		r3,[r0,#0]
+	LDRH		r4,[r0,#2]
+	LDRH		r5,[r1,#0]
+	LDRH		r6,[r1,#2]
+	MUL			r3,r5,r3
+	MUL			r4,r6,r4
+	ADD			r3,r4,r3
+	STR			r3,[r2,#0]
+}
--- test.h
+++ test.h
@@ -2,15 +2,18 @@
 #define __Test
 
 #include <stdint.h>
-void add (uint64_t *a, uint64_t *b, uint64_t *c);
+void add (uint16_t *a, uint16_t *b, uint16_t *c);
 
 __asm void MUL_4(int32_t *a, int32_t *b, int32_t *c);
 __asm void SUM_4(int32_t *a, int32_t *b, int32_t *c);
 __asm void MIN_4(int32_t *a, int32_t *b, int32_t *c);
 __asm void DIV_4(int32_t *a, int32_t *b, int32_t *c);
 
-__asm void MUL_9(int32_t *a, int32_t *b, int32_t *c);
+//__asm void MUL_9(int32_t *a, int32_t *b, int32_t *c);
 __asm void SUM_9(int32_t *a, int32_t *b, int32_t *c);
 __asm void MIN_9(int32_t *a, int32_t *b, int32_t *c);
-__asm void DIV_9(int32_t *a, int32_t *b, int32_t *c);
+//__asm void DIV_9(int32_t *a, int32_t *b, int32_t *c);
+
+__asm void MAC_4(int32_t *a, int32_t *b, int32_t *c);
+
 #endif
